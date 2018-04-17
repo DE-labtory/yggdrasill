27801f72779d8fb25ec49b85c4e2ddc8448c15eb
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2018-04-18 03:26:07 +0900

    empty implementation of default_validator

--- validator/default_validator.go
+++ validator/default_validator.go
@@ -1,1 +1,26 @@
 package validator
+
+import tx "github.com/it-chain/yggdrasill/transaction"
+
+type merkleNode struct {
+	Hash []byte
+	Tx   *tx.DefaultTransaction
+}
+
+type MerkleTree struct {
+	merkleRoot []byte
+	root       *merkleNode
+	leafs      []*merkleNode
+}
+
+func (t *MerkleTree) Validate() bool {
+	return false
+}
+
+func (t *MerkleTree) ValidateTransaction(proof []byte, tx *tx.Transaction) bool {
+	return false
+}
+
+func (t *MerkleTree) GetProof() []byte {
+	return t.merkleRoot
+}
