262f11cee902e5ee842a91960e1c915b4a9f4895
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2018-04-19 20:22:32 +0900

    refactor testing for validator

--- validator/default_validator_test.go
+++ validator/default_validator_test.go
@@ -9,103 +9,24 @@ import (
 )
 
 func TestNewMerkleTree(t *testing.T) {
-	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
-	testingTime, _ := time.Parse(longForm, "Feb 3, 2013 at 7:54pm (PST)")
+	testData := getTestingData(0)
 
-	type args struct {
-		txList []*tx.DefaultTransaction
-	}
 	tests := []struct {
 		name         string
-		args         args
+		txList       []*tx.DefaultTransaction
 		wantRootHash []byte
 		wantErr      bool
 	}{
 		{
-			name: "Create new merkle tree",
-			args: args{
-				txList: []*tx.DefaultTransaction{
-					&tx.DefaultTransaction{
-						InvokePeerID:      "p01",
-						TransactionID:     "tx01",
-						TransactionStatus: 0,
-						TransactionType:   0,
-						TransactionHash:   "hashValue",
-						TimeStamp:         testingTime,
-						TxData: &tx.TxData{
-							Jsonrpc: "jsonRPC01",
-							Method:  "invoke",
-							Params: tx.Params{
-								ParamsType: 0,
-								Function:   "function01",
-								Args:       []string{"arg1", "arg2"},
-							},
-							ID: "txdata01",
-						},
-					},
-					&tx.DefaultTransaction{
-						InvokePeerID:      "p02",
-						TransactionID:     "tx02",
-						TransactionStatus: 0,
-						TransactionType:   0,
-						TransactionHash:   "hashValue",
-						TimeStamp:         testingTime,
-						TxData: &tx.TxData{
-							Jsonrpc: "jsonRPC02",
-							Method:  "invoke",
-							Params: tx.Params{
-								ParamsType: 0,
-								Function:   "function02",
-								Args:       []string{"arg1", "arg2"},
-							},
-							ID: "txdata02",
-						},
-					},
-					&tx.DefaultTransaction{
-						InvokePeerID:      "p03",
-						TransactionID:     "tx03",
-						TransactionStatus: 0,
-						TransactionType:   0,
-						TransactionHash:   "hashValue",
-						TimeStamp:         testingTime,
-						TxData: &tx.TxData{
-							Jsonrpc: "jsonRPC03",
-							Method:  "invoke",
-							Params: tx.Params{
-								ParamsType: 0,
-								Function:   "function03",
-								Args:       []string{"arg1", "arg2"},
-							},
-							ID: "txdata03",
-						},
-					},
-					&tx.DefaultTransaction{
-						InvokePeerID:      "p04",
-						TransactionID:     "tx04",
-						TransactionStatus: 0,
-						TransactionType:   0,
-						TransactionHash:   "hashValue",
-						TimeStamp:         testingTime,
-						TxData: &tx.TxData{
-							Jsonrpc: "jsonRPC04",
-							Method:  "invoke",
-							Params: tx.Params{
-								ParamsType: 0,
-								Function:   "function04",
-								Args:       []string{"arg1", "arg2"},
-							},
-							ID: "txdata04",
-						},
-					},
-				}},
+			name:         "Create new merkle tree",
+			txList:       testData,
 			wantRootHash: []byte{101, 200, 55, 65, 195, 166, 219, 48, 181, 132, 201, 148, 122, 187, 113, 151, 196, 136, 178, 241, 183, 21, 166, 213, 54, 196, 27, 116, 45, 204, 153, 17},
 			wantErr:      false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
-			got, err := NewMerkleTree(convertType(tt.args.txList))
+			got, err := NewMerkleTree(convertType(tt.txList))
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NewMerkleTree() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -118,7 +39,9 @@ func TestNewMerkleTree(t *testing.T) {
 }
 
 func TestMerkleTree_Serialize(t *testing.T) {
-	_, merkleTree, _ := createTestingMerkleTree(0)
+	testData := getTestingData(0)
+	merkleTree, _ := NewMerkleTree(convertType(testData))
+
 	tests := []struct {
 		name string
 		t    *MerkleTree
@@ -145,11 +68,9 @@ func TestMerkleTree_Serialize(t *testing.T) {
 }
 
 func TestMerkleTree_Validate(t *testing.T) {
-	testData, merkleTree, _ := createTestingMerkleTree(0)
-	convTestData := make([]tx.Transaction, 0)
-	for _, tx := range testData {
-		convTestData = append(convTestData, tx)
-	}
+	testData := getTestingData(0)
+	merkleTree, _ := NewMerkleTree(convertType(testData))
+	convTestData := convertType(testData)
 
 	tests := []struct {
 		name string
@@ -170,13 +91,27 @@ func TestMerkleTree_Validate(t *testing.T) {
 }
 
 func TestMerkleTree_ValidateTransaction(t *testing.T) {
-	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
-	testingTime, _ := time.Parse(longForm, "Feb 3, 2013 at 7:54pm (PST)")
-	testData, merkleTree, _ := createTestingMerkleTree(0)
-	convTestData := make([]tx.Transaction, 0)
-	for _, tx := range testData {
-		convTestData = append(convTestData, tx)
+	notIncludedTxTime, _ := time.Parse("Jan 2, 2006 at 3:04pm (MST)", "Feb 3, 2013 at 7:54pm (PST)")
+	notIncludedTx := &tx.DefaultTransaction{
+		InvokePeerID:      "p05",
+		TransactionID:     "tx05",
+		TransactionStatus: 0,
+		TransactionType:   0,
+		TransactionHash:   "hashValue",
+		TimeStamp:         notIncludedTxTime,
+		TxData: &tx.TxData{
+			Jsonrpc: "jsonRPC05",
+			Method:  "invoke",
+			Params: tx.Params{
+				ParamsType: 0,
+				Function:   "function05",
+				Args:       []string{"arg1", "arg2"},
+			},
+			ID: "txdata05",
+		},
 	}
+	testData := getTestingData(0)
+	merkleTree, _ := NewMerkleTree(convertType(testData))
 
 	tests := []struct {
 		name string
@@ -190,24 +125,7 @@ func TestMerkleTree_ValidateTransaction(t *testing.T) {
 		},
 		{
 			name: "Test false ValidationTransaction",
-			t: &tx.DefaultTransaction{
-				InvokePeerID:      "p05",
-				TransactionID:     "tx05",
-				TransactionStatus: 0,
-				TransactionType:   0,
-				TransactionHash:   "hashValue",
-				TimeStamp:         testingTime,
-				TxData: &tx.TxData{
-					Jsonrpc: "jsonRPC05",
-					Method:  "invoke",
-					Params: tx.Params{
-						ParamsType: 0,
-						Function:   "function05",
-						Args:       []string{"arg1", "arg2"},
-					},
-					ID: "txdata05",
-				},
-			},
+			t:    notIncludedTx,
 			want: false,
 		},
 	}
@@ -220,11 +138,11 @@ func TestMerkleTree_ValidateTransaction(t *testing.T) {
 	}
 }
 
-func createTestingMerkleTree(index int) ([]*tx.DefaultTransaction, *MerkleTree, error) {
+func getTestingData(index int) []*tx.DefaultTransaction {
 	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
 	testingTime, _ := time.Parse(longForm, "Feb 3, 2013 at 7:54pm (PST)")
 
-	testData := [][]*tx.DefaultTransaction{
+	return [][]*tx.DefaultTransaction{
 		[]*tx.DefaultTransaction{
 			&tx.DefaultTransaction{
 				InvokePeerID:      "p01",
@@ -299,11 +217,7 @@ func createTestingMerkleTree(index int) ([]*tx.DefaultTransaction, *MerkleTree, 
 				},
 			},
 		},
-	}
-
-	merkleTree, error := NewMerkleTree(convertType(testData[index]))
-
-	return testData[index], merkleTree, error
+	}[index]
 }
 
 func convertType(txList []*tx.DefaultTransaction) []tx.Transaction {
