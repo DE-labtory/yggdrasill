86f874b2295580dbff1349d991af1102343928f9
byron1st <byron1st@icloud.com>
2018-04-26 23:51:32 +0900

    Revert "define basic Block struct and Manager interface"
    This reverts commit 01711e1bfd946f19740135a91274bc33a29ae251.

--- block/block.go
+++ block/block.go
@@ -4,20 +4,13 @@ import (
 	"errors"
 
 	tx "github.com/it-chain/yggdrasill/transaction"
-	"github.com/it-chain/yggdrasill/validator"
 )
 
-// ErrInvalidTransactionType 에러는 Block에 들어온 Transaction의 Type이 의도한 것이 아닐 때 반환된다.
-var ErrInvalidTransactionType = errors.New("Invalid Transaction Type Error")
+var InvalidTransactionTypeError = errors.New("Invalid Transaction Pointer Type Error")
 
-// Block 구조체는 Block의 구조를 정의한다. Block의 구조를 확장하고 싶으면 BlockInf를 구현하여야 한다.
-type Block struct {
-	Proof [][]byte
-	Manager
-}
-
-// Manager 인터페이스는 Block을 관리하기 위한 기능을 제공하는 함수들을 모아둔 인터페이스이다.
-type Manager interface {
+//interface에 맞춰 설계
+//interface를 implement하는 모든 custom block을 사용 가능하게 구현.
+type Block interface {
 	PutTransaction(transaction tx.Transaction) error
 	Serialize() ([]byte, error)
 	GenerateHash() error
@@ -25,26 +18,4 @@ type Manager interface {
 	GetTransactions() []tx.Transaction
 	GetHeight() uint64
 	IsPrev(serializedBlock []byte) bool
-}
-
-// SetProof 함수는 Block의 Proof 값을 주어진 Validator로 계산하여 할당한다.
-func (b Block) SetProof(txList []tx.Transaction, validator validator.Validator) error {
-	proof, error := validator.BuildProof(txList)
-	if error != nil {
-		return error
-	}
-
-	b.Proof = proof
-
-	return nil
-}
-
-// NewBlock 함수는 새로운 Block을 생성한다.
-func NewBlock(txList []tx.Transaction, customBlockManager Manager, validator validator.Validator) Block {
-	newBlock := Block{}
-
-	newBlock.SetProof(txList, validator)
-	newBlock.Manager = customBlockManager
-
-	return newBlock
 }
