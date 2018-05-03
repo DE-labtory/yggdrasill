3d30fa42356ccf5284f9dee6b754d35e0081b210
byron1st <byron1st@icloud.com>
2018-05-03 13:32:25 +0900

    Add Signature stuff to Transaction interface

--- common/transaction.go
+++ common/transaction.go
@@ -1,9 +1,17 @@
 package common
 
-// Transaction interface는 it-chain에서 사용되는 모든 Transaction이 공통적으로 가져야 하는 함수들을 정의함.
+// Transaction interface는 Transaction이 공통적으로 가져야 하는 함수들을 정의함.
 type Transaction interface {
+	// Transaction의 required field getters
 	GetID() string
+	GetContent() []byte
+	GetSignature() []byte
+
+	// Transaction의 required field setters
 	CalculateSeal() ([]byte, error)
+	SetSignature([]byte)
+
+	// Transaction의 저장을 위한 []byte로 변환 및 재변환
 	Serialize() ([]byte, error)
 	Deserialize(serializedBytes []byte) error
 }
