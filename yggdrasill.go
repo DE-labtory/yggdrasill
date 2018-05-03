ce6fa8ee149bd55fe56d0b425cdb5dede23e4885
byron1st <byron1st@icloud.com>
2018-05-03 11:35:40 +0900

    Refactor code in Yggdrasill.go and Add comments for each exported function

--- yggdrasill.go
+++ yggdrasill.go
@@ -27,6 +27,7 @@ type Yggdrasill struct {
 	validator  common.Validator
 }
 
+// NewYggdrasill 함수는 새로운 Yggdrasill 객체를 생성한다. keyValueDB와 validator는 필수이며, opts는 현재 지원되지 않는다.
 func NewYggdrasill(keyValueDB key_value_db.KeyValueDB, validator common.Validator, opts map[string]interface{}) (*Yggdrasill, error) {
 	if keyValueDB == nil || validator == nil {
 		return nil, ErrNoRequiredParameters
@@ -37,10 +38,12 @@ func NewYggdrasill(keyValueDB key_value_db.KeyValueDB, validator common.Validato
 	return &Yggdrasill{DBProvider: dbProvider, validator: validator}, nil
 }
 
+// Close 함수는 Yggdrasill 객체의 DB를 닫는다.
 func (y *Yggdrasill) Close() {
 	y.DBProvider.Close()
 }
 
+// AddBlock 함수는 새로운 Block을 Yggdrasill의 DB에 저장한다. 저장하기 전에 validator로 Block을 검증한다.
 func (y *Yggdrasill) AddBlock(block common.Block) error {
 	serializedBlock, err := block.Serialize()
 	if err != nil {
@@ -92,14 +95,81 @@ func (y *Yggdrasill) AddBlock(block common.Block) error {
 	return nil
 }
 
+// GetBlockByHeight 함수는 Yggdrasill 객체에 저장된 Block을 height 값으로 찾아 반환한다.
+func (y *Yggdrasill) GetBlockByHeight(block common.Block, height uint64) error {
+	blockHeightDB := y.DBProvider.GetDBHandle(blockHeightDB)
+
+	blockSeal, err := blockHeightDB.Get([]byte(fmt.Sprint(height)))
+	if err != nil {
+		return err
+	}
+
+	return y.GetBlockBySeal(block, blockSeal)
+}
+
+// GetBlockBySeal 함수는 Yggdrasill 객체에 저장된 Block을 seal 값으로 찾아 반환한다.
+func (y *Yggdrasill) GetBlockBySeal(block common.Block, seal []byte) error {
+	blockSealDB := y.DBProvider.GetDBHandle(blockSealDB)
+
+	serializedBlock, err := blockSealDB.Get(seal)
+	if err != nil {
+		return err
+	}
+
+	err = block.Deserialize(serializedBlock)
+
+	return err
+}
+
+// GetBlockByTxID 함수는 Yggdrasill 객체에 저장된 Block을 Transaction의 ID 값으로 찾아 반환한다.
+func (y *Yggdrasill) GetBlockByTxID(block common.Block, txID string) error {
+	utilDB := y.DBProvider.GetDBHandle(utilDB)
+
+	blockSeal, err := utilDB.Get([]byte(txID))
+
+	if err != nil {
+		return err
+	}
+
+	return y.GetBlockBySeal(block, blockSeal)
+}
+
+// GetLastBlock 함수는 Yggdrasill 객체에 저장된 마지막 block을 반환한다.
+func (y *Yggdrasill) GetLastBlock(block common.Block) error {
+	utilDB := y.DBProvider.GetDBHandle(utilDB)
+
+	serializedBlock, err := utilDB.Get([]byte(lastBlockKey))
+	if serializedBlock == nil || err != nil {
+		return err
+	}
+
+	err = block.Deserialize(serializedBlock)
+
+	return err
+}
+
+// GetTransactionByTxID 함수는 Yggdrasill 객체에 저장된 Block 안에 저장된 Transaction을 ID 값으로 찾아 반환한다.
+func (y *Yggdrasill) GetTransactionByTxID(transaction common.Transaction, txID string) error {
+	transactionDB := y.DBProvider.GetDBHandle(transactionDB)
+
+	serializedTX, err := transactionDB.Get([]byte(txID))
+	if err != nil {
+		return err
+	}
+
+	err = transaction.Deserialize(serializedTX)
+
+	return err
+}
+
 func (y *Yggdrasill) validateBlock(block common.Block) error {
 	if y.validator == nil {
 		return ErrNoValidator
 	}
 
-	utilDB := y.DBProvider.GetDBHandle(UTIL_DB)
+	utilDB := y.DBProvider.GetDBHandle(utilDB)
 
-	lastBlockByte, err := utilDB.Get([]byte(LAST_BLOCK_KEY))
+	lastBlockByte, err := utilDB.Get([]byte(lastBlockKey))
 	if err != nil {
 		return err
 	}
@@ -131,66 +201,4 @@ func (y *Yggdrasill) validateBlock(block common.Block) error {
 	}
 
 	return nil
-}
-
-func (y *Yggdrasill) GetBlockByHeight(block common.Block, height uint64) error {
-	blockHeightDB := y.DBProvider.GetDBHandle(blockHeightDB)
-
-	blockSeal, err := blockHeightDB.Get([]byte(fmt.Sprint(height)))
-	if err != nil {
-		return err
-	}
-
-	return y.GetBlockBySeal(block, blockSeal)
-}
-
-func (y *Yggdrasill) GetBlockBySeal(block common.Block, seal []byte) error {
-	blockSealDB := y.DBProvider.GetDBHandle(blockSealDB)
-
-	serializedBlock, err := blockSealDB.Get(seal)
-	if err != nil {
-		return err
-	}
-
-	err = block.Deserialize(serializedBlock)
-
-	return err
-}
-
-func (y *Yggdrasill) GetLastBlock(block common.Block) error {
-	utilDB := y.DBProvider.GetDBHandle(utilDB)
-
-	serializedBlock, err := utilDB.Get([]byte(lastBlockKey))
-	if serializedBlock == nil || err != nil {
-		return err
-	}
-
-	err = block.Deserialize(serializedBlock)
-
-	return err
-}
-
-func (y *Yggdrasill) GetTransactionByTxID(transaction common.Transaction, txID string) error {
-	transactionDB := y.DBProvider.GetDBHandle(transactionDB)
-
-	serializedTX, err := transactionDB.Get([]byte(txID))
-	if err != nil {
-		return err
-	}
-
-	err = transaction.Deserialize(serializedTX)
-
-	return err
-}
-
-func (y *Yggdrasill) GetBlockByTxID(block common.Block, txID string) error {
-	utilDB := y.DBProvider.GetDBHandle(utilDB)
-
-	blockSeal, err := utilDB.Get([]byte(txID))
-
-	if err != nil {
-		return err
-	}
-
-	return y.GetBlockBySeal(block, blockSeal)
 }
